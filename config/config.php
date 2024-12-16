45b04abca4c8ff615ed4610da984e5bc49642a82
Alhammanazil <alhammanazil18@gmail.com>
2024-12-16 16:17:54 +0700

    add: function for generate and validate token

--- config/config.php
+++ config/config.php
@@ -12,3 +12,20 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+// Fungsi untuk generate dan validasi token
+if (!function_exists('generateToken')) {
+  function generateToken($id)
+  {
+    $key = 'secret_key_anda'; // Kunci rahasia untuk enkripsi
+    return base64_encode(openssl_encrypt($id, 'AES-128-ECB', $key));
+  }
+}
+
+if (!function_exists('validateToken')) {
+  function validateToken($token)
+  {
+    $key = 'secret_key_anda'; // Kunci rahasia yang sama untuk dekripsi
+    return openssl_decrypt(base64_decode($token), 'AES-128-ECB', $key);
+  }
+}
